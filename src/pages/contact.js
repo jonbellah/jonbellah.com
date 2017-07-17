f2664c6285b375ba52d7a68128de3bf8a798d515
Jon Bellah <jbellah@gmail.com>
2017-07-17 14:20:32 -0600

    Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,9 +12,9 @@ export default class Contact extends React.Component {
 
 				<div className="page__content page__content--contact container--narrow">
 					<h3>Near Denver? Let's grab a beer.</h3>
-					<p>No? That's cool, too. I still want to chat! The best way to get in touch is to follow me on Twitter at <a href="https://twitter.com/jonbellah">@jonbellah</a>. If you'd like to send an email, you can reach me at:</p>
-					<a href="#">
-						hello (at) jonbellah (dot) com
+					<p>No? That's cool, too. I still want to chat! The best way to get in touch is to follow me on Twitter at <a href="https://twitter.com/jonbellah">@jonbellah</a>. If you'd rather send an email, though, you can reach me at:</p>
+					<a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#106;&#111;&#110;&#098;&#101;&#108;&#108;&#097;&#104;&#046;&#099;&#111;&#109;" className="contact__link">
+						&#104;&#101;&#108;&#108;&#111;&#064;&#106;&#111;&#110;&#098;&#101;&#108;&#108;&#097;&#104;&#046;&#099;&#111;&#109;
 					</a>
 				</div>
 			</div>
