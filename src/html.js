a0788efb413a4f89629306a490c80d7eddfca2f6
Jon Bellah <jbellah@gmail.com>
2017-07-19 22:15:50 -0600

    One more back out

--- src/html.js
+++ src/html.js
@@ -1,32 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Html extends Component {
+export default class Html extends Component {
+	static propTypes = {
+		body: PropTypes.string,
+	}
+
 	render() {
 		return (
 			<html lang="en">
 				<head>
 					{this.props.headComponents}
 					<meta charSet="utf-8" />
-					<meta name="description" content="Jon is a full-stack web developer, speaker, and occasional writer" />
 					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 					<title>JonBellah.com</title>
 				</head>
 				<body>
-					<div
-						id="___gatsby"
-						dangerouslySetInnerHTML={{ __html: this.props.body }}
-					/>
+					<div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
 					{this.props.postBodyComponents}
 				</body>
 			</html>
 		)
 	}
 }
-
-Html.propTypes = {
-	body: PropTypes.string,
-};
-
-export default Html;
