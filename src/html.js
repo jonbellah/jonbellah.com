dfae703f2f844eb7e098e7201ff3750480a92e20
Jon Bellah <jbellah@gmail.com>
2017-07-19 22:40:59 -0600

    Refactor the html.js component

--- src/html.js
+++ src/html.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Html extends Component {
-	static propTypes = {
-		body: PropTypes.string,
-	}
+const Html = (props) => {
+	return(
+		<html lang="en">
+			<head>
+				{props.headComponents}
+				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</head>
+			<body>
+				<div id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
+				{props.postBodyComponents}
+			</body>
+		</html>
+	)
+};
 
-	render() {
-		return (
-			<html lang="en">
-				<head>
-					{this.props.headComponents}
-					<meta charSet="utf-8" />
-					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-					<title>JonBellah.com</title>
-				</head>
-				<body>
-					<div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
-					{this.props.postBodyComponents}
-				</body>
-			</html>
-		)
-	}
-}
+Html.propTypes = {
+	body: PropTypes.string,
+};
+
+
+export default Html;
