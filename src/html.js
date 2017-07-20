9dd6be9e3fe36af00ce69bed0f68b21f8ff0e3c5
Jon Bellah <jbellah@gmail.com>
2017-07-19 22:47:09 -0600

    Revert

--- src/html.js
+++ src/html.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const HTML = (props) => {
-	return(
-		<html lang="en">
-			<head>
-				{props.headComponents}
-				<meta charSet="utf-8" />
-				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			</head>
-			<body>
-				<div id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
-				{props.postBodyComponents}
-			</body>
-		</html>
-	)
-};
+export default class HTML extends Component {
+	static propTypes = {
+		body: PropTypes.string,
+	}
 
-export default HTML;
+	render() {
+		return (
+			<html lang="en">
+				<head>
+					<title>JonBellah.com</title>
+					{this.props.headComponents}
+					<meta charSet="utf-8" />
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				</head>
+				<body>
+					<div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
+					{this.props.postBodyComponents}
+				</body>
+			</html>
+		)
+	}
+}
