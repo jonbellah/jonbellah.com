9f010dae7ba02ad7ff3299b695b09b6867be8351
Jon Bellah <jbellah@gmail.com>
2017-07-19 20:32:10 -0600

    Working on server render updates

--- src/html.js
+++ src/html.js
@@ -12,17 +12,11 @@ export default class HTML extends Component {
 		return (
 			<html lang="en">
 				<head>
+					{this.props.headComponents}
 					<meta charSet="utf-8" />
 					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-					<meta property="og:locale" content="en_US" />
-					<meta property="og:type" content="website" />
-					<meta property="og:site_name" content="Jon Bellah" />
-					<meta name="theme-color" content="#EAB75C" />
-					<link rel="icon" href={favicon} type="x-icon/image" />
-					<link rel="manifest" href="/manifest.json" />
 					<title>JonBellah.com</title>
-					{this.props.headComponents}
 				</head>
 				<body>
 					<div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
