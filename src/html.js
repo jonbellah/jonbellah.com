7b168fee4e3a488b6db1f697f01bea174b51eb47
Jon Bellah <jbellah@gmail.com>
2017-07-20 20:43:12 -0600

    Update html

--- src/html.js
+++ src/html.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default class Html extends Component {
+const propTypes = {
+	headComponents: PropTypes.node.isRequired,
+	body: PropTypes.node.isRequired,
+	postBodyComponents: PropTypes.node.isRequired,
+}
+
+class Html extends Component {
 	render() {
 		return (
 			<html lang="en">
 				<head>
+					{this.props.headComponents}
 					<meta charSet="utf-8" />
+					<meta name="description" content="JonBellah.com - Lead Front-End Engineer @ 10up" />
 					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-					{this.props.headComponents}
+					<title>JonBellah.com</title>
 				</head>
 				<body>
-					<div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
+					<div
+						id="___gatsby"
+						dangerouslySetInnerHTML={{ __html: this.props.body }}
+					/>
 					{this.props.postBodyComponents}
 				</body>
 			</html>
@@ -20,8 +31,6 @@ export default class Html extends Component {
 	}
 }
 
-Html.propTypes = {
-	headComponents: PropTypes.node.isRequired,
-	body: PropTypes.node.isRequired,
-	postBodyComponents: PropTypes.node.isRequired,
-}
+Html.propTypes = propTypes;
+
+module.exports = Html;
