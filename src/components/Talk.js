cc1a9195d186f3a5db35bdef9d38d70c98c3a7bc
Jon Bellah <jbellah@gmail.com>
2017-07-22 12:06:47 -0600

    Past talks layout

--- src/components/Talk.js
+++ src/components/Talk.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
 const Talk = (props) => {
-	const { conf, date, location, talk } = props;
+	const { conf, date, location, talk, link, slides, video } = props;
+	const talkLink = link ? <a href={link}>Link</a> : false;
+	const slidesLink = slides ? <Link to={slides}>Slides</Link> : false;
+	const videoLink = video ? <a href={video}>Video</a> : false;
 
 	return (
 		<div className="talk">
-			<strong>{conf}</strong>
-			<p>{date}</p>
-			<p>{location}</p>
-			<p>{talk}</p>
+			<strong className="talk__event">{conf}</strong>
+			<span className="talk__date">{date}</span>
+			<span className="talk__location">{location}</span>
+			<span className="talk__title">{talk}</span>
+
+			<div className="talk__links">
+				{talkLink}
+				{slidesLink}
+				{videoLink}
+			</div>
 		</div>
 	);
 };
--- src/pages/speaking.js
+++ src/pages/speaking.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 
 import talks from '../data/talks';
@@ -16,11 +15,20 @@ const Speaking = () => {
 			</header>
 
 			<div className="page__content container--narrow">
-				I provide talks and workshops on web design and development. These days, I primarly focus on JavaScript, with a particular focus on React and its surrounding ecosystem. If you're interested in having me speak at your event, please get in touch at <a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#106;&#111;&#110;&#098;&#101;&#108;&#108;&#097;&#104;&#046;&#099;&#111;&#109;">&#104;&#101;&#108;&#108;&#111;&#064;&#106;&#111;&#110;&#098;&#101;&#108;&#108;&#097;&#104;&#046;&#099;&#111;&#109;</a>.
+				<p className="page__subhead">
+					I provide talks and workshops on web design and development. These days, I primarly focus on JavaScript, with a particular focus on React and its surrounding ecosystem. If you're interested in having me speak at your event, please get in touch at <a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#106;&#111;&#110;&#098;&#101;&#108;&#108;&#097;&#104;&#046;&#099;&#111;&#109;">&#104;&#101;&#108;&#108;&#111;&#064;&#106;&#111;&#110;&#098;&#101;&#108;&#108;&#097;&#104;&#046;&#099;&#111;&#109;</a>.
+				</p>
+
 				<h3>Biography</h3>
-				
+				<p>Jon began writing code at an early age, when his parents bought their first family computer. His interest grew out of a desire to build websites for his various gaming interests (mostly StarCraft), so with the help of Geocities, Microsoft FrontPage, and some HTML Goodies, he started on the journey that eventually led him to WordPress.</p>
+
+				<p>Jon has been working with WordPress for several years now. In that time, he has worked with a variety of clients ranging from small, Mom and Pop businesses to Fortune 500 enterprises.</p>
+
+				<p>Prior to joining 10up, Jon was Director of Web Development at a boutique advertising agency near Dallas, TX. In early 2014, Jon launched a small premium theme shop that he’s since sold to the crew at UpThemes.</p>
+
+				<p>In addition to WordPress, Jon has also spent time tinkering with Ruby on Rails, Node, Backbone, and Angular projects over the last few years.</p>
 				<h3>Previous Talks</h3>
-				<div className="talks__container">
+				<div className="talks">
 					{talkList}
 				</div>
 			</div>
