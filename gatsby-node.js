de2b7ce327b95f68bb85d9d2652522eb017c9ae9
Jon Bellah <jbellah@gmail.com>
2017-07-29 09:42:08 -0600

    Code cleanup and headers updates

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,31 +2,33 @@ const Shell = require('child_process');
 const path = require('path');
 
 exports.onCreateNode = function ({ node, boundActionCreators, getNode }) {
-	const { createNodeField } = boundActionCreators
-	let slug
-	if (node.internal.type === `MarkdownRemark`) {
-		const fileNode = getNode(node.parent)
-		const parsedFilePath = path.parse(fileNode.relativePath)
-		if (parsedFilePath.name !== `index` && parsedFilePath.dir !== ``) {
-			slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`
-		} else if (parsedFilePath.dir === ``) {
-			slug = `/${parsedFilePath.name}/`
+	const { createNodeField } = boundActionCreators;
+
+	let slug;
+
+	if (node.internal.type === 'MarkdownRemark') {
+		const fileNode = getNode(node.parent);
+		const parsedFilePath = path.parse(fileNode.relativePath);
+		if (parsedFilePath.name !== 'index' && parsedFilePath.dir !== '') {
+			slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`;
+		} else if (parsedFilePath.dir === '') {
+			slug = `/${parsedFilePath.name}/`;
 		} else {
-			slug = `/${parsedFilePath.dir}/`
+			slug = `/${parsedFilePath.dir}/`;
 		}
 
 		// Add slug as a field on the node.
-		createNodeField({ node, name: `slug`, value: slug })
+		createNodeField({ node, name: 'slug', value: slug });
 	}
-}
+};
 
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
 exports.createPages = ({ graphql, boundActionCreators }) => {
-	const { createPage } = boundActionCreators
+	const { createPage } = boundActionCreators;
 
 	return new Promise((resolve, reject) => {
-		const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+		const blogPostTemplate = path.resolve('src/templates/blog-post.js');
 		// Query for markdown nodes to use in creating pages.
 		resolve(
 			graphql(
@@ -42,10 +44,10 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
 					}
 				}
 			}
-		`
-			).then(result => {
+		`,
+			).then((result) => {
 				if (result.errors) {
-					reject(result.errors)
+					reject(result.errors);
 				}
 
 				// Create pages for each markdown file.
@@ -58,18 +60,18 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
 						// as a GraphQL variable to query for data from the markdown file.
 						context: {
 							path,
-						}
-					})
-				})
-			})
-		)
-	})
-}
+						},
+					});
+				});
+			}),
+		);
+	});
+};
 
 // Copy redirects on build
-exports.onPostBuild = function() {
+exports.onPostBuild = function () {
 	Shell.execSync('cp src/_redirects public');
 	Shell.execSync('cp src/_headers public');
 	Shell.execSync('cp src/manifest.json public');
 	Shell.execSync('cp -R src/images/icons public/icons');
-}
+};
