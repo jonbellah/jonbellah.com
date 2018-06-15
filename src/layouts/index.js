28b424c0dc2d64378407c6ea2d07b8a4343b6460
Jon Bellah <jon@ted.com>
2018-06-14 20:13:39 -0600

    Update packages, add prettierrc, add youtube link

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -12,109 +12,133 @@ import favicon from '../images/favicon.ico';
 import Logo from '../images/Logo';
 import TwitterIcon from '../images/TwitterIcon';
 import GithubIcon from '../images/GithubIcon';
+import YouTubeIcon from '../images/YouTubeIcon';
 import RssIcon from '../images/RssIcon';
 
 export default class Template extends Component {
-	static propTypes = {
-		children: PropTypes.func,
-	}
+  static propTypes = {
+    children: PropTypes.func,
+  };
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			mobileMenuOpen: false,
-		};
+    this.state = {
+      mobileMenuOpen: false,
+    };
 
-		this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
-		this.closeMobileMenu = this.closeMobileMenu.bind(this);
-	}
+    this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
+    this.closeMobileMenu = this.closeMobileMenu.bind(this);
+  }
 
-	toggleMobileMenu() {
-		this.setState({
-			mobileMenuOpen: !this.state.mobileMenuOpen,
-		});
-	}
+  toggleMobileMenu() {
+    this.setState({
+      mobileMenuOpen: !this.state.mobileMenuOpen,
+    });
+  }
 
-	closeMobileMenu() {
-		this.setState({
-			mobileMenuOpen: false,
-		});
-	}
+  closeMobileMenu() {
+    this.setState({
+      mobileMenuOpen: false,
+    });
+  }
 
-	render() {
-		const menuClass = this.state.mobileMenuOpen ? 'active' : '';
-		const currentYear = new Date().getFullYear();
+  render() {
+    const menuClass = this.state.mobileMenuOpen ? 'active' : '';
+    const currentYear = new Date().getFullYear();
 
-		return (
-			<div>
-				<Helmet
-					title="JonBellah.com"
-					meta={[
-						{ name: 'description', content: 'Jon is a full-stack web developer, speaker, and occasional writer' },
-					]}
-				>
-					<meta property="og:locale" content="en_US" />
-					<meta property="og:type" content="website" />
-					<meta property="og:site_name" content="Jon Bellah" />
-					<meta name="theme-color" content="#EAB75C" />
-					<link rel="icon" href={favicon} type="x-icon/image" />
-					<link rel="manifest" href="/manifest.json" />
-				</Helmet>
-				<div className="site">
-					<header id="header" className="site-header animated fadeInDown">
-						<div className="container">
-							<Link to="/" className="site-header__link">
-								<Logo className="site-header__logo" />
-							</Link>
-							<button id="mobile-toggle" className={`toggle-icon ${menuClass}`} aria-label="Mobile menu" onClick={this.toggleMobileMenu}>
-								<span className="screen-reader-text">Menu</span>
-								<span className="line line-1" />
-								<span className="line line-2" />
-								<span className="line line-3" />
-							</button>
+    return (
+      <div>
+        <Helmet
+          title="JonBellah.com"
+          meta={[
+            {
+              name: 'description',
+              content:
+                'Jon is a full-stack web developer, speaker, and occasional writer',
+            },
+          ]}
+        >
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="Jon Bellah" />
+          <meta name="theme-color" content="#EAB75C" />
+          <link rel="icon" href={favicon} type="x-icon/image" />
+          <link rel="manifest" href="/manifest.json" />
+        </Helmet>
+        <div className="site">
+          <header id="header" className="site-header animated fadeInDown">
+            <div className="container">
+              <Link to="/" className="site-header__link">
+                <Logo className="site-header__logo" />
+              </Link>
+              <button
+                id="mobile-toggle"
+                className={`toggle-icon ${menuClass}`}
+                aria-label="Mobile menu"
+                onClick={this.toggleMobileMenu}
+              >
+                <span className="screen-reader-text">Menu</span>
+                <span className="line line-1" />
+                <span className="line line-2" />
+                <span className="line line-3" />
+              </button>
 
-							<nav id="navigation" className={`site-nav ${menuClass}`}>
-								<NavLink to="/articles/" className="site-nav__item" onClick={this.closeMobileMenu}>
-									Articles
-								</NavLink>
-								<NavLink to="/speaking/" className="site-nav__item" onClick={this.closeMobileMenu}>
-									Speaking
-								</NavLink>
-								<NavLink to="/contact/" className="site-nav__item" onClick={this.closeMobileMenu}>
-									Contact
-								</NavLink>
-							</nav>
-						</div>
-					</header>
+              <nav id="navigation" className={`site-nav ${menuClass}`}>
+                <NavLink
+                  to="/articles/"
+                  className="site-nav__item"
+                  onClick={this.closeMobileMenu}
+                >
+                  Articles
+                </NavLink>
+                <NavLink
+                  to="/speaking/"
+                  className="site-nav__item"
+                  onClick={this.closeMobileMenu}
+                >
+                  Speaking
+                </NavLink>
+                <NavLink
+                  to="/contact/"
+                  className="site-nav__item"
+                  onClick={this.closeMobileMenu}
+                >
+                  Contact
+                </NavLink>
+              </nav>
+            </div>
+          </header>
 
-					<main className="site-content">
-						{this.props.children()}
-					</main>
+          <main className="site-content">{this.props.children()}</main>
 
-					<footer id="colophon" className="site-footer animated fadeInUp">
-						<div className="container flex">
-							<div className="site-footer__copyright">
-								&copy; {currentYear} JonBellah.com. Made in Colorado.
-							</div>
+          <footer id="colophon" className="site-footer animated fadeInUp">
+            <div className="container flex">
+              <div className="site-footer__copyright">
+                &copy; {currentYear} JonBellah.com. Made in Colorado.
+              </div>
 
-							<div className="site-footer__social">
-								<a href="https://twitter.com/jonbellah">
-									<TwitterIcon />
-								</a>
+              <div className="site-footer__social">
+                <a href="https://youtube.com/jonbellah">
+                  <YouTubeIcon />
+                </a>
 
-								<a href="https://github.com/jonbellah">
-									<GithubIcon />
-								</a>
+                <a href="https://twitter.com/jonbellah">
+                  <TwitterIcon />
+                </a>
 
-								<a href="/rss.xml">
-									<RssIcon />
-								</a>
-							</div>
-						</div>
-					</footer>
-				</div>
-			</div>
-		);
-	}
+                <a href="https://github.com/jonbellah">
+                  <GithubIcon />
+                </a>
+
+                <a href="/rss.xml">
+                  <RssIcon />
+                </a>
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    );
+  }
 }
