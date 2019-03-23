a42a0eefa33ad120a0540d3407ecfaf737cc033a
Jon Bellah <jon@ted.com>
2019-03-23 14:44:36 -0600

    Version updates, copy updates

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -19,13 +19,13 @@ const Contact = () => (
 
     <div className="page__content page__content--courses container--narrow">
       <p className="page__subhead">
-        If you're a front-end developer interested in leveling up, you've come
-        to the right place!
+        If you're a front-end developer interested in leveling up your skills,
+        you've come to the right place!
       </p>
 
       <p>
         I'm hard at work on new courses, so keep an eye out here. In the
-        meantime, check out my state machines course.
+        meantime, I'd love for you to check out my state machines course.
       </p>
 
       <h3>Learn State Machines</h3>
@@ -37,8 +37,8 @@ const Contact = () => (
       </p>
 
       <p>
-        Together, we will take an application called Photobook from comp
-        to working application. The app has many of the features that you would
+        Together, we will take an application called Photobook from comp to
+        working application. The app has many of the features that you would
         expect to find in apps today: authentication, data fetching, CRUD
         actions, automated tests, and more.
       </p>
