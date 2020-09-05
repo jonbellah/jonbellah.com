c44d1ea3380d3d9b2087162785228b80180f505f
Jon Bellah <jon@ted.com>
2020-09-05 13:02:52 -0600

    Fix bio

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
 
-{
-  /* <div itemProp="author" itemScope itemType="http://schema.org/Person" className="max-w-3xl mx-auto">
-<div>
-  <img itemProp="image" src="/avatar.jpg" alt="Jon Bellah" />
-</div>
-
-<div>
-  <span itemProp="name">Jon Bellah</span>
-  <div itemProp="description">
-    
-  </div>
-</div>
-</div> */
-}
-
 const Bio: React.FC = () => (
   <div className="flex flex-wrap items-center justify-center sm:justify-left border-t border-gray-200 w-full pt-10 sm:px-1 max-w-3xl mx-auto">
     <figure className="px-2 mb-1 sm:mb-0 w-full sm:w-1/5 flex justify-center">
       <img
         itemProp="image"
-        src="/avatar.jpg"
+        src="/images/avatar.jpg"
         alt="Jon Bellah"
         className="rounded-full p-4 sm:p-0"
       />
